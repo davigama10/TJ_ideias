5883386efc0a0ca83346c5952b70896f7482e7f1
davigama10 <davigama10@gmail.com>
2023-06-19 16:05:26 -0300

    classe grupo com construtor

--- script.js
+++ script.js
@@ -1,41 +1,53 @@
-let unidades = [];
-let semanas = [];
-let sorteio = [];
-
-for (let i = 1; i <= 50; i++) {
-  unidades.push("unidade " + i);
-  semanas.push("semana " + i);
+class Grupo {
+    constructor() {
+        this.unidades = [];
+        this.semanas = [];
+        this.sorteio = [];
+        this.unidadesSorteio = [];
+        this.semanasSorteio = [];
+    }
 }
 
-let unidadesSorteio = [...unidades];
-let semanasSorteio = [...semanas];
+let grupo1 = new Grupo;
+
+
+for (let i = 1; i <= 50; i++) {
+  grupo1.unidades.push("unidade " + i);
+  grupo1.semanas.push("semana " + i);
+}
+
+grupo1.unidadesSorteio = [...grupo1.unidades];
+grupo1.semanasSorteio = [...grupo1.semanas];
+
+console.log(grupo1.unidadesSorteio);
+console.log(grupo1.semanasSorteio);
 
 exibirArrays();
 
 function sortearUnidadeParaSemana() {
-    let semanaIndex = Math.floor(Math.random() * semanasSorteio.length);
-    let unidadeIndex = Math.floor(Math.random() * unidadesSorteio.length);
-    let semana = semanasSorteio[semanaIndex];
-    let unidade = unidadesSorteio[unidadeIndex];
-    semanasSorteio.splice(semanaIndex, 1);
-    unidadesSorteio.splice(unidadeIndex, 1);
+    let semanaIndex = Math.floor(Math.random() * grupo1.semanasSorteio.length);
+    let unidadeIndex = Math.floor(Math.random() * grupo1.unidadesSorteio.length);
+    let semana = grupo1.semanasSorteio[semanaIndex];
+    let unidade = grupo1.unidadesSorteio[unidadeIndex];
+    grupo1.semanasSorteio.splice(semanaIndex, 1);
+    grupo1.unidadesSorteio.splice(unidadeIndex, 1);
     let sorteioItem = semana + " - " + unidade;
-    sorteio.push(sorteioItem);
+    grupo1.sorteio.push(sorteioItem);
     exibirSorteio();
     exibirArrays();
 }
 
 function exibirArrays() {
     let arrayUnidades = document.getElementById("arrayUnidades");
-    arrayUnidades.innerText = unidadesSorteio.join(", ")
+    arrayUnidades.innerText = grupo1.unidadesSorteio.join(", ")
 
     let arraySemanas = document.getElementById("arraySemanas");
-    arraySemanas.innerText = semanasSorteio.join(", ")
+    arraySemanas.innerText = grupo1.semanasSorteio.join(", ")
 }
 
 function exibirSorteio() {
     let arraySorteio = document.getElementById("arraySorteio");
-    arraySorteio.innerText = sorteio.join(", ")
+    arraySorteio.innerText = grupo1.sorteio.join(", ")
 }
 
 let sortearButton = document.getElementById("sortearButton");
