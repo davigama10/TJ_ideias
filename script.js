a9cf6680950fff53f5bb441c4c37d30a681aaa11
davigama10 <davigama10@gmail.com>
2023-06-20 21:06:52 -0300

    opção de selecionar grupo

--- script.js
+++ script.js
@@ -5,51 +5,118 @@ class Grupo {
         this.sorteio = [];
         this.unidadesSorteio = [];
         this.semanasSorteio = [];
+        this.preencherGrupo();
+        this.criarCopia();
+    }
+
+    preencherGrupo() {
+        for (let i = 1; i <= 50; i++) {
+            this.unidades.push("unidade " + i);
+            this.semanas.push("semana " + i);
+        }
+    }
+
+    criarCopia() {
+        this.unidadesSorteio = [...this.unidades];
+        this.semanasSorteio = [...this.semanas];
     }
 }
 
 let grupo1 = new Grupo;
+let grupo2 = new Grupo;
+let grupo3 = new Grupo;
+let grupo4 = new Grupo;
+let grupo5 = new Grupo;
 
+function utilizarGrupoSelecionado() {
+    let select = document.getElementById("grupos");
+    let grupoSelecionado = select.value;
 
-for (let i = 1; i <= 50; i++) {
-  grupo1.unidades.push("unidade " + i);
-  grupo1.semanas.push("semana " + i);
+    let paragrafo = document.getElementById("grupo-selecionado");
+    paragrafo.textContent = grupoSelecionado;
+  
+    switch (grupoSelecionado) {
+      case "grupo1":
+        exibirArrays(grupo1);
+        exibirSorteio(grupo1);
+        console.log("Grupo 1 selecionado");
+        break;
+      case "grupo2":
+        exibirArrays(grupo2);
+        exibirSorteio(grupo2);
+        console.log("Grupo 2 selecionado");
+        break;
+      case "grupo3":
+        exibirArrays(grupo3);
+        exibirSorteio(grupo3);
+        console.log("Grupo 3 selecionado");
+        break;
+      case "grupo4":
+        exibirArrays(grupo4);
+        exibirSorteio(grupo4);
+        console.log("Grupo 4 selecionado");
+        break;
+      case "grupo5":
+        exibirArrays(grupo5);
+        exibirSorteio(grupo5);
+        console.log("Grupo 5 selecionado");
+        break;
+      default:
+        console.log("Nenhum grupo selecionada");
+    }
 }
 
-grupo1.unidadesSorteio = [...grupo1.unidades];
-grupo1.semanasSorteio = [...grupo1.semanas];
-
-console.log(grupo1.unidadesSorteio);
-console.log(grupo1.semanasSorteio);
-
-exibirArrays();
-
-function sortearUnidadeParaSemana() {
-    let semanaIndex = Math.floor(Math.random() * grupo1.semanasSorteio.length);
-    let unidadeIndex = Math.floor(Math.random() * grupo1.unidadesSorteio.length);
-    let semana = grupo1.semanasSorteio[semanaIndex];
-    let unidade = grupo1.unidadesSorteio[unidadeIndex];
-    grupo1.semanasSorteio.splice(semanaIndex, 1);
-    grupo1.unidadesSorteio.splice(unidadeIndex, 1);
+function sortearUnidadeParaSemana(grupo) {
+    let semanaIndex = Math.floor(Math.random() * grupo.semanasSorteio.length);
+    let unidadeIndex = Math.floor(Math.random() * grupo.unidadesSorteio.length);
+    let semana = grupo.semanasSorteio[semanaIndex];
+    let unidade = grupo.unidadesSorteio[unidadeIndex];
+    grupo.semanasSorteio.splice(semanaIndex, 1);
+    grupo.unidadesSorteio.splice(unidadeIndex, 1);
     let sorteioItem = semana + " - " + unidade;
-    grupo1.sorteio.push(sorteioItem);
-    exibirSorteio();
-    exibirArrays();
+    grupo.sorteio.push(sorteioItem);
+    exibirSorteio(grupo);
+    exibirArrays(grupo);
 }
 
-function exibirArrays() {
+function exibirArrays(grupo) {
     let arrayUnidades = document.getElementById("arrayUnidades");
-    arrayUnidades.innerText = grupo1.unidadesSorteio.join(", ")
+    arrayUnidades.innerText = grupo.unidadesSorteio.join(", ")
 
     let arraySemanas = document.getElementById("arraySemanas");
-    arraySemanas.innerText = grupo1.semanasSorteio.join(", ")
+    arraySemanas.innerText = grupo.semanasSorteio.join(", ")
 }
 
-function exibirSorteio() {
+function exibirSorteio(grupo) {
     let arraySorteio = document.getElementById("arraySorteio");
-    arraySorteio.innerText = grupo1.sorteio.join(", ")
+    arraySorteio.innerText = grupo.sorteio.join(", ")
+}
+
+function chamarSorteio() {
+    let select = document.getElementById("grupos");
+    let grupoSelecionado = select.value;    
+  
+    switch (grupoSelecionado) {
+      case "grupo1":
+        sortearUnidadeParaSemana(grupo1);
+        break;
+      case "grupo2":
+        sortearUnidadeParaSemana(grupo2);
+        break;
+      case "grupo3":
+        sortearUnidadeParaSemana(grupo3);
+        break;
+      case "grupo4":
+        sortearUnidadeParaSemana(grupo4);
+        break;
+      case "grupo5":
+        sortearUnidadeParaSemana(grupo5);
+        break;
+      default:
+        console.log("Nenhum grupo selecionada");
+    }
 }
 
 let sortearButton = document.getElementById("sortearButton");
-sortearButton.addEventListener("click", sortearUnidadeParaSemana);
+sortearButton.addEventListener("click", chamarSorteio);
 
